bc9e56c67a1f58a0f8a2bc267eca10cce387b50b
Borales <bordun.alexandr@gmail.com>
2012-09-02 00:36:23 +0300

    Added GA code

--- public/themes/classic/views/layouts/main.php
+++ public/themes/classic/views/layouts/main.php
@@ -6,6 +6,21 @@
     <meta name="language" content="en" />
     <link rel="shortcut icon" type="image/x-icon" href="http://static.yiiframework.com/favicon.ico" />
     <title><?=CHtml::encode($this->pageTitle); ?></title>
+    <?php if( !IS_LOCALHOST ) { ?>
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-8920748-6']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+    <?php }?>
 </head>
 <body>
 
